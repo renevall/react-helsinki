f26c914215b0ea013e9b75cbf6bc6afb0e7e037e
Rene Vallecillo <reneval@gmail.com>
2024-03-05 11:47:29 -0800

    whole app to internet

--- part3/tutorial/index.js
+++ part3/tutorial/index.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -72,7 +87,14 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
